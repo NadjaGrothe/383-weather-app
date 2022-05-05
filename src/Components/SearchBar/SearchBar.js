e5dd08ecbc1ef00dfbab0c28ba15cde1b668125e
kevweolomu <kevweolomu@yahoo.ca>
2022-05-05 10:38:21 +0100

    searchbar created

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+    return (
+        <div className="searchbar">
+            <input placeholder="search..."></input>
+        </div>
+    )
+}
+
+export default SearchBar;
